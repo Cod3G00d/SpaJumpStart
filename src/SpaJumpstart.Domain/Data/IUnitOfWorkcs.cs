ec5b596245ace8037e29f46573e672bf70b498dd
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-12-27 13:19:23 +0000

    Fix project structure - add unit of work and repository base

---
+++ src/SpaJumpstart.Domain/Data/IUnitOfWorkcs.cs
@@ -0,0 +1,23 @@
+﻿using SpaJumpstart.Domain.Data.Repositories;
+using SpaJumpstart.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace SpaJumpstart.Domain.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepositoryBase<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+
+        void BeginTransaction();
+
+        int Commit();
+
+        Task<int> CommitAsync();
+
+        void Rollback();
+
+        void Dispose(bool disposing);
+
+    }
+}
---
+++ src/SpaJumpstart.Domain/Data/Repositories/IRepositoryBase.cs
@@ -0,0 +1,26 @@
+﻿using SpaJumpstart.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SpaJumpstart.Domain.Data.Repositories
+{
+    public interface IRepositoryBase<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        void Insert(TEntity entity);
+        void DeleteById(int id);
+        void Delete(TEntity entityToDelete);
+        void Update(TEntity entityToUpdate);
+
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+
+        IEnumerable<TEntity> GetAll();
+        //IQueryable<TEntity> GetAllLazy();
+        //IEnumerable<TEntity> GetAllEager();
+
+        TEntity GetById(int id);
+
+        void Dispose();
+    }
+}
