9ce4fe2a7857f5231f1a170ba9aaca0f0a724b17
dtro-devuk <dtro-devuk@users.noreply.github.com>
2016-01-05 20:25:50 +0000

    remove test project folder

--- tests/SpaJumpstart.WebServices.Unit.Tests/Properties/AssemblyInfo.cs
+++ tests/SpaJumpstart.WebServices.Unit.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpaJumpstart.WebService.Unit.Tests")]
+[assembly: AssemblyTitle("SpaJumpstart.WebServices.Unit.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SpaJumpstart.WebService.Unit.Tests")]
+[assembly: AssemblyProduct("SpaJumpstart.WebServices.Unit.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("650866ef-db48-4813-804e-308c48d71bfd")]
+[assembly: Guid("3a591f47-476e-48b6-a2cc-08003d503728")]
 
 // Version information for an assembly consists of the following four values:
 //
