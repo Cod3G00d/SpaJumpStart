a0ddcf4ea6c23a138f210ba10670ca78b3ecdc12
dtro-devuk <dtro-devuk@users.noreply.github.com>
2016-01-08 18:00:35 +0000

    Spa app fixes, gulp, bower and node modules

---
+++ src/SpaJS.A.Web/gulpfile.config.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var GulpConfig = (function () {
+    function gulpConfig() {
+
+        this.bowerDir = './bower_components';
+        this.targetViewsDir = './Views/Home/Index.cshtml';
+    }
+
+    return gulpConfig;
+})();
+module.exports = GulpConfig;
